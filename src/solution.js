2026f2bdeff18afa0f07ca6d498ec5152934fe4d
Jacek programmer <jacek301@gmail.com>
2024-06-10 23:09:14 +0200

    get character from ASCII Value kata solution

--- src/solution.js
+++ src/solution.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getChar = void 0;
 function getChar(c) {
-    return "";
+    return String.fromCharCode(c);
 }
 exports.getChar = getChar;
