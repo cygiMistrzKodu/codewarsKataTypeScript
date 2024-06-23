f1191a20dfcae1e53099442ece36367e0d06f6c5
Jacek programmer <jacek301@gmail.com>
2024-06-23 15:41:39 +0200

    Filter out the geese kata solution

--- src/solution.js
+++ src/solution.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.gooseFilter = void 0;
 function gooseFilter(birds) {
     const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    // return an array containing all of the strings in the input array except those that match strings in geese
-    return [];
+    return birds.filter(bird => !geese.includes(bird));
 }
 exports.gooseFilter = gooseFilter;
--- src/solution.ts
+++ src/solution.ts
@@ -1,5 +1,5 @@
-export function gooseFilter (birds: string[]): string[] {
+export function gooseFilter(birds: string[]): string[] {
   const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-  // return an array containing all of the strings in the input array except those that match strings in geese
-  return []
+
+  return birds.filter(bird => !geese.includes(bird))
 }
