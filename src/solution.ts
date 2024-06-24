c59ba536a38555f5286ee92b44aebdd81ef4bfc6
Jacek programmer <jacek301@gmail.com>
2024-06-24 17:13:26 +0200

    Exclamation marks series #1: Remove an exclamation mark from the end of string kata start

--- src/solution.ts
+++ src/solution.ts
@@ -1,5 +1,3 @@
-export function gooseFilter(birds: string[]): string[] {
-  const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-
-  return birds.filter(bird => !geese.includes(bird))
+export function remove(s: string): string {
+  throw new Error("The method or operation is not implemented.");
 }
--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -1,14 +1,13 @@
-const gooseFilter = require('../src/solution').gooseFilter;
+import { remove } from "../src/solution";
 import { assert } from "chai";
 
-describe("Basic tests",function(){
-  it("Mixed list",function(){
-    assert.deepEqual(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]),["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
-  });
-  it("No geese",function(){
-    assert.deepEqual(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]),["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]);
-  });
-  it("All geese",function(){
-    assert.deepEqual(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]),[]);
+describe("Basic Tests", function(){
+  it("It should works for basic tests", function(){
+    assert.strictEqual(remove("Hi!") , "Hi");
+    assert.strictEqual(remove("Hi!!!") ,"Hi!!");
+    assert.strictEqual(remove("!Hi") , "!Hi");
+    assert.strictEqual(remove("!Hi!") , "!Hi");
+    assert.strictEqual(remove("Hi! Hi!") , "Hi! Hi");
+    assert.strictEqual(remove("Hi") , "Hi");
   });
 });
