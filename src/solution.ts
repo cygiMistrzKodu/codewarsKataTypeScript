afd6e69c137d52e4a0af864c4e2bc65cb8fdfda3
Jacek programmer <jacek301@gmail.com>
2024-06-26 13:28:51 +0200

    Convert to Binary kata  start

--- src/solution.ts
+++ src/solution.ts
@@ -1,11 +1,3 @@
-export function factorial(n: number) {
-  if (n == 0 || n == 1) {
-    return 1;
-  }
-
-  let factorial = 1;
-  for (let i = 1; i <= n; i++) {
-    factorial *= i;
-  }
-  return factorial;
+export function toBinary(n:number):number {
+  return n //Your code here
 }
--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -1,10 +1,12 @@
 import { assert } from "chai";
-import { factorial } from "../src/solution";
+
+import { toBinary } from "../src/solution";
 
 describe("Basic tests", () => {
-  it("Testing for 0", () => assert.strictEqual(factorial(0), 1));
-  it("Testing for 1", () => assert.strictEqual(factorial(1), 1));
-  it("Testing for 4", () => assert.strictEqual(factorial(4), 24));
-  it("Testing for 7", () => assert.strictEqual(factorial(7), 5040));
-  it("Testing for 17", () => assert.strictEqual(factorial(17), 355687428096000));
-});
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(toBinary(1), 1);
+    assert.strictEqual(toBinary(2), 10);
+    assert.strictEqual(toBinary(3), 11);
+    assert.strictEqual(toBinary(5), 101);  
+  });
+})
