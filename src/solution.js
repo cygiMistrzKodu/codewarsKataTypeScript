ffa57d224d4f56f63a62888953a048d06a4a01b3
Jacek programmer <jacek301@gmail.com>
2024-06-26 18:20:06 +0200

    Maximum Length Difference kata  solution

--- src/solution.js
+++ src/solution.js
@@ -2,7 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mxdiflg = void 0;
 const mxdiflg = (a1, a2) => {
-    // your code
-    return -1;
+    let maxDiffrence = -1;
+    let wordLengthDiffrence = 0;
+    a1.forEach((word1) => {
+        a2.forEach((word2) => {
+            wordLengthDiffrence = Math.abs(word1.length - word2.length);
+            if (wordLengthDiffrence > maxDiffrence) {
+                maxDiffrence = wordLengthDiffrence;
+            }
+        });
+    });
+    return maxDiffrence;
 };
 exports.mxdiflg = mxdiflg;
--- src/solution.ts
+++ src/solution.ts
@@ -1,4 +1,20 @@
-export const mxdiflg = (a1:string[], a2:string[]): number => {
-  // your code
-  return -1
+export const mxdiflg = (a1: string[], a2: string[]): number => {
+
+  let maxDiffrence = -1;
+  let wordLengthDiffrence = 0;
+  a1.forEach((word1) => {
+
+    a2.forEach((word2) => {
+
+      wordLengthDiffrence = Math.abs(word1.length - word2.length);
+      if (wordLengthDiffrence > maxDiffrence) {
+        maxDiffrence = wordLengthDiffrence;
+      }
+
+    });
+
+  });
+
+
+  return maxDiffrence;
 }
