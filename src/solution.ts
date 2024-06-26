a17843a8dd7227e434a7208abc66e95adfed1ae8
Jacek programmer <jacek301@gmail.com>
2024-06-26 18:22:06 +0200

    Ones and Zeros kata  start

--- src/solution.ts
+++ src/solution.ts
@@ -1,20 +1,3 @@
-export const mxdiflg = (a1: string[], a2: string[]): number => {
-
-  let maxDiffrence = -1;
-  let wordLengthDiffrence = 0;
-  a1.forEach((word1) => {
-
-    a2.forEach((word2) => {
-
-      wordLengthDiffrence = Math.abs(word1.length - word2.length);
-      if (wordLengthDiffrence > maxDiffrence) {
-        maxDiffrence = wordLengthDiffrence;
-      }
-
-    });
-
-  });
-
-
-  return maxDiffrence;
-}
+export function binaryArrayToNumber(arr: number[]): number{
+  return 0
+};
--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -1,18 +1,11 @@
-import {mxdiflg} from '../src/solution'
-import {assert} from "chai";
+import { assert } from "chai";
+import { binaryArrayToNumber } from "../src/solution";
 
-
-describe("Fixed Tests", function() {
-    it("Basic tests mxdiflg", function() {        
-        
-        let s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
-        let s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
-        assert.equal(mxdiflg(s1, s2), 13);
-        s1 = ["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"];
-        s2 = ["bbbaaayddqbbrrrv"];
-        assert.equal(mxdiflg(s1, s2), 10);
-        s2 = [];
-        s1 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
-        assert.equal(mxdiflg(s1, s2), -1);
-    });
+describe("One's and Zero's", () => {
+  it("Example tests", () => {
+      assert.strictEqual(binaryArrayToNumber([0,0,0,1]), 1);
+      assert.strictEqual(binaryArrayToNumber([0,0,1,0]), 2);
+      assert.strictEqual(binaryArrayToNumber([1,1,1,1]), 15);
+      assert.strictEqual(binaryArrayToNumber([0,1,1,0]), 6);
+  });
 });
