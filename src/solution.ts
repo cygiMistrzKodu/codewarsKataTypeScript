c6f7582a9e38400def1cb40780a00d34dfe47840
Jacek programmer <jacek301@gmail.com>
2024-06-26 13:11:11 +0200

    Factorial kata  start

--- src/solution.ts
+++ src/solution.ts
@@ -1,16 +1,3 @@
-export const nbYear = (p0: number, percent: number, aug: number, p: number): number => {
-
-  let populationState = p0;
-  const growthEachYear = percent / 100;
-  const commingOrLeavingFactor = aug;
-  const expectedState = p;
-
-  let years = 0;
-  while (expectedState > populationState) {
-    populationState += Math.floor(populationState * growthEachYear + commingOrLeavingFactor);
-    years++;
-
-  }
-
-  return years;
+export function factorial(n: number) {
+  // your code here
 }
--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -1,18 +1,10 @@
-import {nbYear} from '../src/solution'
-import {assert} from "chai";
+import { assert } from "chai";
+import { factorial } from "../src/solution";
 
-function testing(p0:number, percent:number, aug:number, p:number, expected:number) {
-    assert.strictEqual(nbYear(p0, percent, aug, p), expected);
-}
-
-describe("Fixed Tests", function() {
-    it("Basic tests nbYear", function() {
-        testing(1500, 5, 100, 5000, 15);
-        testing(1500000, 2.5, 10000, 2000000, 10);
-        testing(1500000, 0.25, 1000, 2000000, 94);
-        testing(1500000, 0.25, -1000, 2000000, 151);
-        testing(1500000, 0.25, -1000, 2000000, 151);
-        testing(1000, 2, 50, 1214, 4);
-        testing(1500000, 0, 10000, 2000000, 50);
-    });
+describe("Basic tests", () => {
+  it("Testing for 0", () => assert.strictEqual(factorial(0), 1));
+  it("Testing for 1", () => assert.strictEqual(factorial(1), 1));
+  it("Testing for 4", () => assert.strictEqual(factorial(4), 24));
+  it("Testing for 7", () => assert.strictEqual(factorial(7), 5040));
+  it("Testing for 17", () => assert.strictEqual(factorial(17), 355687428096000));
 });
