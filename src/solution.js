aaa1145dac340921b45a60868d462664b404159c
Jacek programmer <jacek301@gmail.com>
2024-06-28 19:24:37 +0200

    Find Nearest square number kata other solution

--- src/solution.js
+++ src/solution.js
@@ -2,25 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.nearestSq = void 0;
 function nearestSq(n) {
-    if (Number.isInteger(Math.sqrt(n))) {
-        return n;
-    }
-    let leftSideSquare = 0;
-    for (let number = n; number > 0; number--) {
-        if (Number.isInteger(Math.sqrt(number))) {
-            leftSideSquare = number;
-            break;
-        }
-    }
-    let squareLeftSideDistance = n - leftSideSquare;
-    let rightSideSquare = 0;
-    for (let number = n;; number++) {
-        if (Number.isInteger(Math.sqrt(number))) {
-            rightSideSquare = number;
-            break;
-        }
-    }
-    let squareRightSideDistance = rightSideSquare - n;
-    return squareLeftSideDistance < squareRightSideDistance ? leftSideSquare : rightSideSquare;
+    return Math.round(n ** 0.5) ** 2;
 }
 exports.nearestSq = nearestSq;
--- src/solution.ts
+++ src/solution.ts
@@ -1,28 +1,4 @@
 export function nearestSq(n: number): number {
 
-  if (Number.isInteger(Math.sqrt(n))) {
-    return n;
-  }
-
-  let leftSideSquare = 0;
-  for (let number = n; number > 0; number--) {
-    if (Number.isInteger(Math.sqrt(number))) {
-      leftSideSquare = number;
-      break;
-    }
-  }
-  let squareLeftSideDistance = n - leftSideSquare;
-
-  let rightSideSquare = 0;
-  for (let number = n; ; number++) {
-    if (Number.isInteger(Math.sqrt(number))) {
-      rightSideSquare = number;
-      break;
-    }
-  }
-
-  let squareRightSideDistance = rightSideSquare - n;
-
-
-  return squareLeftSideDistance < squareRightSideDistance ? leftSideSquare : rightSideSquare
+ return  Math.round(n ** 0.5) ** 2;
 }
