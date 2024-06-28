d6d893d46beed7360ee1eeec7d8202bd72a298ac
Jacek programmer <jacek301@gmail.com>
2024-06-28 18:26:50 +0200

    Find Nearest square number kata start

--- src/solution.ts
+++ src/solution.ts
@@ -1,8 +1,3 @@
-export function cubeChecker(volume: number, side: number): boolean {
-
-  if (volume <= 0 || side <= 0) {
-    return false;
-  }
-
-  return volume === side * side * side;
+export function nearestSq(n : number) : number {
+  return 0 // your code here
 }
--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -1,15 +1,14 @@
+// See https://www.chaijs.com for how to use Chai.
 import { assert } from "chai";
-import { cubeChecker } from "../src/solution";
 
-describe("Cube?", function() {
-  it("Basic tests", function() {
-      assert.isTrue(cubeChecker(1, 1));
-      assert.isTrue(cubeChecker(8, 2));
-      assert.isTrue(cubeChecker(27, 3));
-      assert.isFalse(cubeChecker(0, 35));
-      assert.isFalse(cubeChecker(452, 1));
-      assert.isFalse(cubeChecker(785, 0));
-      assert.isFalse(cubeChecker(367, -18));
-      assert.isFalse(cubeChecker(0, 0));
-  });
+import { nearestSq } from "../src/solution";
+
+describe("Basic tests",() => {
+  it('basic tests', () => {
+    assert.equal(nearestSq(1), 1);
+    assert.equal(nearestSq(2), 1);
+    assert.equal(nearestSq(10), 9);
+    assert.equal(nearestSq(111), 121);
+    assert.equal(nearestSq(9999), 10000);
+  })
 });
