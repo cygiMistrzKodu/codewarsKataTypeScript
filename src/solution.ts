1ee96bf3d6a1c6c4288aae189e81317940b0288d
Jacek programmer <jacek301@gmail.com>
2024-06-30 02:33:19 +0200

    Lario and Muigi Pipe Problem kata start

--- src/solution.ts
+++ src/solution.ts
@@ -1,6 +1,3 @@
-export function predictAge(age1: number, age2: number, age3: number, age4: number, age5: number, age6: number, age7: number, age8: number): number {
-
-  const ages = [age1, age2, age3, age4, age5, age6, age7, age8];
-  return Math.floor(Math.sqrt(ages.map(number => number * number).reduce((sum, number) => sum + number)) / 2);
-
+export function pipeFix(numbers: number[]): number[]{
+  return []
 }
--- test/tests.spec.ts
+++ test/tests.spec.ts
@@ -1,9 +1,13 @@
-import {predictAge} from "../src/solution";
-import {assert} from "chai";
+import { assert } from "chai";
+import { pipeFix } from "../src/solution";
 
-describe("predictAge", function() {
-  it("Sample tests", function() {
-    assert.equal(predictAge(65,60,75,55,60,63,64,45), 86);
-    assert.equal(predictAge(32,54,76,65,34,63,64,45), 79);
+describe("Lario and Muigi Pipe Problem", () => {
+  it("Fixed tests", () => {
+    assert.deepEqual(pipeFix([1,2,3,5,6,8,9]),[1,2,3,4,5,6,7,8,9],'You must return an array from 1-9');
+    assert.deepEqual(pipeFix([1,2,3,12]),[1,2,3,4,5,6,7,8,9,10,11,12],'You must return an array from 1-12');
+    assert.deepEqual(pipeFix([6,9]),[6,7,8,9],'You must return an array from 6-9');
+    assert.deepEqual(pipeFix([-1,4]),[-1,0,1,2,3,4],'You must return an array from -1-4');
+    assert.deepEqual(pipeFix([1,2,3]),[1,2,3],'You must return an array from 1-3');
+    assert.deepEqual(pipeFix([2]),[2]);
   });
 });
